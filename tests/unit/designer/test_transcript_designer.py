3e17200ac529451cf5b4a9f3187833827ca03b3b
josephhjoo <josephjoo@berkeley.edu>
2024-11-06 12:25:12 -0800

    finished

--- tests/unit/designer/test_transcript_designer.py
+++ tests/unit/designer/test_transcript_designer.py
@@ -7,3 +7,4 @@ Test the reverse-translation of protein sequences into optimized DNA.
 Ensure proper RBS assignment for each mRNA.
 Validate handling of codon optimization and RNA folding requirements.
 """
+
